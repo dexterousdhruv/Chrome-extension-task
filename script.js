291eaebf97b6efc902d3d6a9b03f1195fc15f136
dexterousdhruv <dhruvverma0110@gmail.com>
2024-10-01 19:30:16 +0530

    Minor tweak

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heading = document.querySelector(".container h1")
 const result = document.querySelector("#result")
 const showTitleBtn = document.querySelector("#btn")
 
-function showTitle(title, tabs) {
+function showTitle(title) {
   console.log(tabs)
   heading.textContent = "Tab Title"
   result.textContent = title
@@ -15,6 +15,6 @@ chrome.tabs.query(
   { active: true, currentWindow: true },
   function (tabs) {
     const activeTabTitle = tabs[0].title;
-    showTitleBtn.addEventListener('click', () => showTitle(activeTabTitle, tabs))
+    showTitleBtn.addEventListener('click', () => showTitle(activeTabTitle))
   }
 )
